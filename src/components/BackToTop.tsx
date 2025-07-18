34a39af650390e7d54e4f2e329b5215695cfbedd
harshita <harshita.singh@mcbirdtechnologies.com>
2025-07-19 01:59:37 +0530

    back to top button

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const BackToTop: React.FC = () => {
+const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 0) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    // Check visibility on mount
+    toggleVisibility();
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
   return (
-    <a href="#" className="btn btn-lg btn-primary btn-lg-rectangle border-0 back-to-top">
+    <a href="#" className={`btn btn-lg btn-primary btn-lg-rectangle border-0 ${isVisible? "back-to-top":""}`}>
       <i className="bi bi-arrow-up"></i>
     </a>
   );
